8761dc404848c5d1d4a69297ece54d97ded259cf
skaimid <zhuzhaoxin2016@gmail.com>
2022-05-10 15:09:52 +0800

    add CompParticipate code

---
+++ src/containers/CompParticipate/GraphContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default ({ children, title, width }) => {
+  return (
+    <div
+      className="p-4 bg-white rounded-md
+      border-1 shadow-xl hover:shadow-2xl flex flex-col w-full grow shrink-0 m-4"
+    >
+      <div className="text-center text-2xl font-semibold">{title}</div>
+      <div className="p-8 ">{children}</div>
+    </div>
+  );
+};
--- src/containers/CompParticipate/index.js
+++ src/containers/CompParticipate/index.js
@@ -1,32 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HolyGrailLayout from "../../components/HolyGrailLayout";
 import { Select } from "antd";
 import { AutoComplete } from "antd";
-import { Card } from "antd";
 
-import ReactEChartsCore from "echarts-for-react";
-import * as echarts from "echarts/core";
-import { GridComponent } from "echarts/components";
-import { LineChart } from "echarts/charts";
-import { UniversalTransition } from "echarts/features";
-import { CanvasRenderer } from "echarts/renderers";
-
-
-const getOption = () => ({
-  xAxis: {
-    type: "category",
-    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [150, 230, 224, 218, 135, 147, 260],
-      type: "line",
-    },
-  ],
-});
+import Pie from "./pie";
+import Line from "./line";
+import GraphContainer from "./GraphContainer";
 
 const { Option } = Select;
 
@@ -38,54 +17,86 @@ function onSearch(val) {
   console.log("search:", val);
 }
 
-const SelectTest = () => (
-  <Select
-    showSearch
-    placeholder="Select a person"
-    optionFilterProp="children"
-    onChange={onChange}
-    onSearch={onSearch}
-    filterOption={(input, option) =>
-      option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-    }
-  >
-    <Option value="jack">Jack</Option>
-    <Option value="lucy">Lucy</Option>
-    <Option value="tom">Tom</Option>
-  </Select>
-);
-
-const options = [
-  { value: "Burns Bay Road" },
-  { value: "Downing Street" },
-  { value: "Wall Street" },
-];
-
-const Complete = () => (
+const Complete = ({ info, setSelectedComp }) => (
   <AutoComplete
     style={{ width: 200 }}
-    options={options}
-    placeholder="try to type `b`"
-    filterOption={(inputValue, option) => {
-      if (!option) {
+    options={info}
+    placeholder="input here"
+    filterOption={(inputValue, item) => {
+      if (!item) {
         return 0;
       }
-      return (
-        option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-      );
+      return item.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1;
+    }}
+    onSelect={(value) => {
+      setSelectedComp(value);
     }}
   />
 );
 
+const findCompInfo = (comp_name, infoList) => {
+  return infoList.find((item) => {
+    return item.comp_name === comp_name;
+  });
+};
+
 export default function CompParticipate() {
+  const [info, setInfo] = useState(undefined);
+  const [graphData, setGraphData] = useState(undefined);
+  const [selectedComp, setSelectedComp] = useState(undefined);
+
+  useEffect(() => {
+    fetch(`/data/CompParticipate/info.json`)
+      .then((res) => res.json())
+      .catch((e) => {
+        console.log(e);
+        setInfo(undefined);
+      })
+      .then((data) => setInfo(data));
+  }, []);
+
+  useEffect(() => {
+    fetch(`/data/CompParticipate/${selectedComp}.json`)
+      .then((res) => res.json())
+      .catch((e) => {
+        console.log(e);
+        setGraphData(undefined);
+      })
+      .then((data) => setGraphData(data));
+  }, [selectedComp]);
+
   return (
     <HolyGrailLayout>
-      <h1>公司参与</h1>
-      <SelectTest></SelectTest>
-      <Complete></Complete>
-      <Card.Grid className="line_a bg-white">
-        <ReactEChartsCore option={getOption()} theme="ThemeStyle" />
-      </Card.Grid>
+      <div className="flex flex-row w-9/12 flex-nowrap">
+        <div className="w-[50%]">
+          <GraphContainer>
+            {info && (
+              <Complete
+                info={info.map((item) => {
+                  return { value: item.comp_name };
+                })}
+                setSelectedComp={setSelectedComp}
+              ></Complete>
+            )}
+          </GraphContainer>
+        </div>
+
+        <GraphContainer>
+          {selectedComp && info ? (
+            <div className="w-[16rem] ">
+              公司名称{findCompInfo(selectedComp, info).comp_name}
+            </div>
+          ) : (
+            <div> no data</div>
+          )}
+        </GraphContainer>
+      </div>
+
+      <div className="m-16 w-72 h-72 bg-white">
+        <Pie />
+      </div>
+
+      {graphData ? <Line data={graphData.ci} /> : <div> no data</div>}
     </HolyGrailLayout>
   );
 }
